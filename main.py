ddb5d4525b64b516eaeaeae48dca99f0ecaa18d6
StarDust <10196469+catsoup@user.noreply.gitee.com>
2025-02-07 23:14:15 +0800

    change main.py

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ class MyPlugin(BasePlugin):
     @handler(PersonNormalMessageReceived)
     async def person_normal_message_received(self, ctx: EventContext):
         msg = ctx.event.text_message  # 这里的 event 即为 PersonNormalMessageReceived 的对象
-        if msg == "hello":  # 如果消息为hello
+        if msg == "hello-neko":  # 如果消息为hello
 
             # 输出调试信息
-            self.ap.logger.debug("hello, {}".format(ctx.event.sender_id))
+            self.ap.logger.debug("hello-neko, {}".format(ctx.event.sender_id))
 
             # 回复消息 "hello, <发送者id>!"
-            ctx.add_return("reply", ["hello, {}!".format(ctx.event.sender_id)])
+            ctx.add_return("reply", ["hello-neko, {}!".format(ctx.event.sender_id)])
 
             # 阻止该事件默认行为（向接口获取回复）
             ctx.prevent_default()
